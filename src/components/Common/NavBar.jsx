f2c0f2a07c9181e5d6c6d66ed6f5a50e31b17fba
AngelSpinazzola <angel_spinazzola@outlook.es>
2025-07-30 14:44:36 -0300

    fix: ahora se guarda el token jwt luego del registro.

--- src/components/Common/NavBar.jsx
+++ src/components/Common/NavBar.jsx
@@ -267,7 +267,7 @@ const NavBar = ({ searchTerm, setSearchTerm, showSearch = false }) => {
                                     <button 
                                         onClick={() => setShowDropdown(!showDropdown)} 
                                         className="p-2 text-nova-gray-400 md:hover:text-nova-cyan transition-colors">
-                                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                         </svg>
                                     </button>
